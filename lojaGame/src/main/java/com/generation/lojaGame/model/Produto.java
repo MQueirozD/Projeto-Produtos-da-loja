b8a97981221f7004d53bf5bcf2e65492156e54e9
Matues <mateus.q.d@gmail.com>
2022-09-06 12:00:07 -0300

    Versão 1.0

---
+++ lojaGame/src/main/java/com/generation/lojaGame/model/Produto.java
@@ -0,0 +1,114 @@
+package com.generation.lojaGame.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+
+@Entity
+@Table(name="tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message= "Nome é Obrigatório!")
+	@Size(min=5,max=100, message = "Nome deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String nome;
+	
+	@NotNull(message= "Descrição é Obrigatório!")
+	private String tipo;	
+		
+	private Boolean multiplay;
+	
+	@UpdateTimestamp
+	private LocalDateTime data;	
+	
+	private Long quantidade;	
+	
+	private Double preco;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produtos")
+	private Categoria categoria;
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public Boolean getMultiplay() {
+		return multiplay;
+	}
+
+	public void setMultiplay(Boolean multiplay) {
+		this.multiplay = multiplay;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+
+	public Long getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Long quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+		
+
+}
